d39d221dab23a383cfe1e0fc3e81da235b2f8b09
KesThav <kesigan.thav@gmail.com>
2022-12-28 13:25:00 +0100

    sequential benchmark done

---
+++ benchmark/functions/benchmark_functions.js
@@ -0,0 +1,69 @@
+export const getUser_GraphQL = () => {
+  const usersQuery = {
+    query: `query {
+      users {
+        name
+        email
+        points
+        _id
+      }
+    }`,
+    variables: {},
+  };
+  const headers = {
+    "content-type": "application/json",
+  };
+  const queryContent = {
+    url: "http://localhost:8082/graphql",
+    method: "post",
+    headers: headers,
+    data: usersQuery,
+  };
+
+  return queryContent;
+};
+
+export const createProduct_GraphQL = () => {
+  const createProductMutation = {
+    query: `mutation addProduct($name:String!,$description:String!,$type:String!,$averageRating:Int, $quantity:Int!,$price:Float!,$images:[String]) {
+    addProduct(name:$name,description:$description,type:$type,averageRating:$averageRating,quantity:$quantity,price:$price,images:$images) {
+      name
+      description
+      type
+      averageRating
+      quantity
+      price
+      images {
+        url
+      }
+    }
+  }`,
+    variables: {
+      name: "V15 Gen 2",
+      description: "'15.60 \", Intel Core i3-1115G4, 8 Go, 256 Go, CH'",
+      type: "Ordinateur portable",
+      averageRating: 4,
+      price: 599,
+      quantity: 10,
+      images: [
+        "https://static.digitecgalaxus.ch/Files/5/2/0/9/4/0/7/7/dfbcfa48-2952-4a94-a27f-0ed09c3312ea.jpg",
+        "https://static.digitecgalaxus.ch/Files/5/2/0/9/4/0/7/8/95763f79-aa6e-426d-8f5e-fedb7fc31ba1.jpg",
+        "https://static.digitecgalaxus.ch/Files/5/2/0/9/4/0/7/9/9e565b50-f254-44e8-b475-ab142c2c84f5.jpg",
+        "https://static.digitecgalaxus.ch/Files/5/2/0/9/4/0/8/0/Lenovo_V15_G2_ITL_CT2_02.jpeg",
+      ],
+    },
+  };
+
+  const headers = {
+    "content-type": "application/json",
+  };
+
+  const queryContent = {
+    url: "http://localhost:8084/graphql",
+    method: "post",
+    headers: headers,
+    data: createProductMutation,
+  };
+
+  return queryContent;
+};
--- benchmark/functions/sequentialBenchmark.js
+++ benchmark/functions/sequentialBenchmark.js
@@ -1,21 +1,14 @@
 import axios from "axios";
 import fs from "fs";
+import {
+  getUser_GraphQL,
+  createProduct_GraphQL,
+} from "./benchmark_functions.js";
 
-const nIterations = [1, 10, 100, 1000];
+const nIterations = [1, 10, 100, 1000, 10000];
 
 export const b_getUsers_GraphQL = async () => {
-  const usersQuery = {
-    query: `query {
-      users {
-        name
-        email
-        points
-        _id
-      }
-    }`,
-    variables: {},
-  };
-
+  const getUserQuery = getUser_GraphQL();
   const results = {
     1: [],
     10: [],
@@ -36,15 +29,9 @@ export const b_getUsers_GraphQL = async () => {
         console.log(
           `Iteration: ${z} - nIterations: ${nIterations[i]} - j: ${j}`
         );
-        const headers = {
-          "content-type": "application/json",
-        };
-        const response = await axios({
-          url: "http://localhost:8082/graphql",
-          method: "post",
-          headers: headers,
-          data: usersQuery,
-        });
+
+        const response = await axios(getUserQuery);
+        console.log(response.data);
         if (response.data.errors) {
           errors++;
         }
@@ -69,36 +56,8 @@ export const b_getUsers_GraphQL = async () => {
 };
 
 export const b_createProduct_GraphQL = async () => {
-  let createProductMutation = {
-    query: `mutation addProduct($name:String!,$description:String!,$type:String!,$averageRating:Int, $quantity:Int!,$price:Float!,$images:[String]) {
-    addProduct(name:$name,description:$description,type:$type,averageRating:$averageRating,quantity:$quantity,price:$price,images:$images) {
-      name
-      description
-      type
-      averageRating
-      quantity
-      price
-      images {
-        url
-      }
-    }
-  }`,
-    variables: {
-      name: "V15 Gen 2",
-      description: "'15.60 \", Intel Core i3-1115G4, 8 Go, 256 Go, CH'",
-      type: "Ordinateur portable",
-      averageRating: 4,
-      price: 599,
-      quantity: 10,
-      images: [
-        "https://static.digitecgalaxus.ch/Files/5/2/0/9/4/0/7/7/dfbcfa48-2952-4a94-a27f-0ed09c3312ea.jpg",
-        "https://static.digitecgalaxus.ch/Files/5/2/0/9/4/0/7/8/95763f79-aa6e-426d-8f5e-fedb7fc31ba1.jpg",
-        "https://static.digitecgalaxus.ch/Files/5/2/0/9/4/0/7/9/9e565b50-f254-44e8-b475-ab142c2c84f5.jpg",
-        "https://static.digitecgalaxus.ch/Files/5/2/0/9/4/0/8/0/Lenovo_V15_G2_ITL_CT2_02.jpeg",
-      ],
-    },
-  };
-
+  const createProductMutation = createProduct_GraphQL();
+  console.log(createProductMutation);
   const results = {
     1: [],
     10: [],
@@ -119,15 +78,8 @@ export const b_createProduct_GraphQL = async () => {
         console.log(
           `Iteration: ${z} - nIterations: ${nIterations[i]} - j: ${j}`
         );
-        const headers = {
-          "content-type": "application/json",
-        };
-        const response = await axios({
-          url: "http://localhost:8084/graphql",
-          method: "post",
-          headers: headers,
-          data: createProductMutation,
-        });
+
+        const response = await axios(createProductMutation);
         if (response.data.errors) {
           errors++;
         }
@@ -191,7 +143,7 @@ export const b_getProductBoughtByUser_GraphQL = async () => {
     errors_1000: [],
     errors_10000: [],
   };
-  for (let z = 0; z < 4; z++) {
+  for (let z = 0; z < 3; z++) {
     for (let i = 0; i < nIterations.length; i++) {
       let errors = 0;
       let time = Date.now();
@@ -398,7 +350,7 @@ export const b_getProductBoughtByUser_REST = async () => {
     errors_1000: [],
     errors_10000: [],
   };
-  for (let z = 0; z < 5; z++) {
+  for (let z = 0; z < 3; z++) {
     for (let i = 0; i < nIterations.length; i++) {
       let errors = 0;
       let time = Date.now();
